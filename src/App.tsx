1e55a1e092c3edd96d956e2738e391a65c9f75c5
Timothy Coleman <timhc22@gmail.com>
2022-11-03 20:22:22 +0000

    Update button

--- src/App.tsx
+++ src/App.tsx
@@ -27,26 +27,11 @@ function App() {
     <div className="App">
 
       <>
-        {/*<ARButton />*/}
-
-        <XRButton
-          className='xr-button'
-          /* The type of `XRSession` to create */
-          mode={'AR'}
-          /**
-           * `XRSession` configuration options
-           * @see https://immersive-web.github.io/webxr/#feature-dependencies
-           */
-          // sessionInit={{ optionalFeatures: ['local-floor', 'bounded-floor', 'hand-tracking', 'layers'] }}
+        <ARButton
           sessionInit={{ optionalFeatures: ['local-floor'] }}
-          /** Whether this button should only enter an `XRSession`. Default is `false` */
           enterOnly={false}
-          /** Whether this button should only exit an `XRSession`. Default is `false` */
           exitOnly={false}
-        >
-          {/* Can accept regular DOM children and has an optional callback with the XR button status (unsupported, exited, entered) */}
-          AR View
-        </XRButton>
+        />
 
         <Canvas linear>
           <XR
