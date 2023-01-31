f94343e131ead22e341aa38219e708349958f407
Tim <tim@unegma.com>
2023-01-31 01:24:15 +0000

    Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,22 @@
 import React, { useRef } from "react";
 // eslint-disable-next-line
-import { XR, Controllers, Hands, ARButton, useHitTest } from "@react-three/xr";
+import { XR, Controllers, Hands, ARButton, useHitTest,  } from "@react-three/xr";
 // eslint-disable-next-line
 import { Box, OrbitControls } from "@react-three/drei";
 import { Canvas } from "@react-three/fiber";
 // eslint-disable-next-line
 import { Matrix4, Mesh } from "three";
 
-
-const HitTestExample = () => {
+export default function App () {
   const ref = useRef(null!);
+
+  let hitpoint = useRef()
 
   useHitTest((hitMatrix: Matrix4, hit: XRHitTestResult) => {
     // @ts-ignore
-    hitMatrix.decompose(ref.position, ref.quaternion, ref.scale);
+    hitMatrix.decompose(ref.current.position, ref.current.quaternion, ref.current.scale);
   });
 
-  return (
-    <Box ref={ref} args={[0.1, 0.1, 0.1]}/>
-  );
-};
-
-export default function App () {
   return (
     <>
       <ARButton sessionInit={{ requiredFeatures: ["hit-test"] }}/>
@@ -29,7 +24,7 @@ export default function App () {
         <XR>
           <ambientLight />
           <pointLight position={[10, 10, 10]} />
-          <HitTestExample />
+          <Box ref={ref} args={[0.1, 0.1, 0.1]}/>
           <Controllers />
         </XR>
       </Canvas>
