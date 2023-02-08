31a14c40d87c6b58d4b52b42cb54389bbc28c204
Tim <tim@unegma.com>
2023-02-08 13:44:17 +0000

    Add king

---
+++ src/App-other.tsx
@@ -0,0 +1,40 @@
+import React, { useRef } from "react";
+// eslint-disable-next-line
+import { XR, Controllers, Hands, ARButton, useHitTest,  } from "@react-three/xr";
+// eslint-disable-next-line
+import { Box, OrbitControls } from "@react-three/drei";
+import { Canvas } from "@react-three/fiber";
+// eslint-disable-next-line
+import { Matrix4, Mesh } from "three";
+
+
+const HitTestExample = () => {
+  const ref = useRef(null!);
+
+  let hitpoint = useRef()
+
+  useHitTest((hitMatrix: Matrix4, hit: XRHitTestResult) => {
+    // @ts-ignore
+    hitMatrix.decompose(ref.current.position, ref.current.quaternion, ref.current.scale);
+  });
+
+  return (
+    <Box ref={hitpoint} args={[0.1, 0.1, 0.1]}/>
+  );
+};
+
+export default function AppOther () {
+  return (
+    <>
+      <ARButton sessionInit={{ requiredFeatures: ["hit-test"] }}/>
+      <Canvas>
+        <XR>
+          <ambientLight />
+          <pointLight position={[10, 10, 10]} />
+          <HitTestExample />
+          <Controllers />
+        </XR>
+      </Canvas>
+    </>
+  );
+};
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App2 from './App2';
+import App from './App';
 
 test('renders learn react link', () => {
-  render(<App2 />);
+  render(<App />);
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
